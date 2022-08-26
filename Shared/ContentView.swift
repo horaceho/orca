1bfc19ddb55a280a280ef2e06a07dc97e812a715
Horace Ho <horaceh@gmail.com>
2022-08-26 11:34:18 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Horace Ho on 26/8/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/OrcaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrcaApp.swift
+//  Shared
+//
+//  Created by Horace Ho on 26/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct OrcaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
